63c2f5daa8bbc39914533d81747e3483aaefd5ff
sathapa <santosh.thapa@janus.com>
2019-10-09 23:17:54 -0400

    psw update'

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 module.exports = {
-    database : 'mongodb+srv://santosh:<password>@meanauthappcluster-c1jjm.mongodb.net/test?retryWrites=true&w=majority',
+    database : 'mongodb+srv://santosh:D5EUowwD4oxwztZw@meanauthappcluster-c1jjm.mongodb.net/test?retryWrites=true&w=majority',
                //'mongodb://localhost:27017/meanauth',
     secret: 'yoursecret'
 }
